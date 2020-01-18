e624a62768d257dce7ac2ca45823e5c0efd02e3e
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2020-01-18 16:46:05 +0100

    updated javals config

--- installs/install_javals.fish
+++ installs/install_javals.fish
@@ -4,6 +4,9 @@ if test -e nvim/lsps/javals/README.md
   echo "[+] java LS already initialised"
 else
   git submodule update --init nvim/lsps/javals
+  if [ ! -e /bin/jlink ]
+    sudo ln -s /usr/bin/jlink /bin/jlink
+  end
   cd nvim/lsps/javals && scripts/link_linux.sh
   cd nvim/lsps/javals && mvn package -DskipTests
   echo "[+] java LS initialised"
