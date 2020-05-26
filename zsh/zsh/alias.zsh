d9f77682243d762061d8f3a6dcef948c0bd974c4
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2020-05-26 16:52:17 +0200

    added better colors for zsh

--- zsh/zsh/alias.zsh
+++ zsh/zsh/alias.zsh
@@ -8,8 +8,8 @@ alias vim="nvim"
 
 # ag / sk
 alias ag="ag --hidden --ignore .git --ignore .cache --color"
-alias sk="sk -m"
-alias ska="sk --ansi -i -c 'ag \"{}\"'"
+alias sk="sk -m --color=${KITTY_THEME},hl:3,spiller:2,fg+:9,hl+:3,selected:6,query:5,matched_bg:-1"
+alias ska="sk -m --color=${KITTY_THEME},hl:3,spiller:2,fg+:9,hl+:3,selected:6,query:5,matched_bg:-1 --ansi -i -c 'ag \"{}\"'"
 
 # ls
 alias l="ls -FA --color=always"
@@ -31,3 +31,9 @@ alias ti="timew"
 
 # tree
 alias tree="tree -C"
+
+# ip
+alias ip="ip -c"
+
+# grep
+alias grep="grep --color=auto"
