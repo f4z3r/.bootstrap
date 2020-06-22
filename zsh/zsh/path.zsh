3da0e64c6ef30bf6912c8c7c215114565ae9499e
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2020-06-22 23:37:28 +0200

    updated path to include perl core

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -2,10 +2,11 @@
 #============================================== Path =============================================
 #=================================================================================================
 
-PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin"
+PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin"                # system bins
 
-[ -d "$HOME/.local/bin/" ] && PATH="$PATH:$HOME/.local/bin/"
-[ -d "$HOME/.nimble/bin" ] && PATH="$PATH:$HOME/.nimble/bin"
-[ -d "$HOME/.cargo/bin" ] && PATH="$PATH:$HOME/.cargo/bin"
+[ -d "/usr/bin/core_perl/" ] && PATH="$PATH:/usr/bin/core_perl/"   # perl core bins
+[ -d "$HOME/.local/bin/" ] && PATH="$PATH:$HOME/.local/bin/"       # local bins
+[ -d "$HOME/.nimble/bin" ] && PATH="$PATH:$HOME/.nimble/bin"       # nim bins
+[ -d "$HOME/.cargo/bin" ] && PATH="$PATH:$HOME/.cargo/bin"         # rust bins
 
 export PATH
