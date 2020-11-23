43f7223a50866b0763db92dacd6d4123996b328a
Jakob Beckmann <jakob.beckmann@bit.admin.ch>
2020-11-23 11:17:49 +0100

    improved rpt to have correct order and use shared history

--- zsh/zsh/env.zsh
+++ zsh/zsh/env.zsh
@@ -10,6 +10,24 @@ if which bat &> /dev/null; then
   export PAGER="bat"
 fi
 
+# ZSH
+setopt INC_APPEND_HISTORY
+setopt sharehistory
+# # set history size
+# export HISTSIZE=10000
+# # save history after logout
+# export SAVEHIST=10000
+# # history file
+# export HISTFILE=~/.zhistory
+# # append into history file
+# setopt INC_APPEND_HISTORY
+# # save only one command if 2 common are same and consistent
+# setopt HIST_IGNORE_DUPS
+# # timestamp for each entry
+# setopt EXTENDED_HISTORY
+
+
+
 # FZF
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --ignore .cache -g ""'
 
