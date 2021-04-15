e44d101d048ccedb0bade8ee40a686f8d3eaf6c9
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-04-15 17:03:46 +0200

    added snap binaries to path

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -9,14 +9,15 @@ export GOPATH
 GOBIN="$GOPATH/bin"
 export GOBIN
 
-PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin"                  # system bins
-[ -d "/usr/bin/core_perl/" ] && PATH="$PATH:/usr/bin/core_perl/"     # perl core bins
-[ -d "/usr/bin/vendor_perl/" ] && PATH="$PATH:/usr/bin/vendor_perl/" # perl vendor bins
-[ -d "$HOME/.local/bin/" ] && PATH="$PATH:$HOME/.local/bin/"         # local bins
-[ -d "$HOME/.nimble/bin" ] && PATH="$PATH:$HOME/.nimble/bin"         # nim bins
-[ -d "$HOME/.cargo/bin" ] && PATH="$PATH:$HOME/.cargo/bin"           # rust bins
-[ -n "$GOBIN" ] && PATH="$PATH:$GOBIN"                               # go bins
-[ -d "$HOME/.asdf" ] && . $HOME/.asdf/asdf.sh                        # asdf bins
+PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin"                        # system bins
+[ -d "/var/lib/snapd/snap/bin/" ] && PATH="$PATH:/var/lib/snapd/snap/bin/" # snap bins
+[ -d "/usr/bin/core_perl/" ] && PATH="$PATH:/usr/bin/core_perl/"           # perl core bins
+[ -d "/usr/bin/vendor_perl/" ] && PATH="$PATH:/usr/bin/vendor_perl/"       # perl vendor bins
+[ -d "$HOME/.local/bin/" ] && PATH="$PATH:$HOME/.local/bin/"               # local bins
+[ -d "$HOME/.nimble/bin" ] && PATH="$PATH:$HOME/.nimble/bin"               # nim bins
+[ -d "$HOME/.cargo/bin" ] && PATH="$PATH:$HOME/.cargo/bin"                 # rust bins
+[ -n "$GOBIN" ] && PATH="$PATH:$GOBIN"                                     # go bins
+[ -d "$HOME/.asdf" ] && . $HOME/.asdf/asdf.sh                              # asdf bins
 export PATH
 
 # also provide local /bin -> /share/man pages
