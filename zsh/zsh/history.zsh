8f350a267e3f810da54019aa0b279a89f3d166ff
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-08-30 21:22:12 +0200

    do not add commands with "password" to command history after cleanup

--- zsh/zsh/history.zsh
+++ zsh/zsh/history.zsh
@@ -9,3 +9,11 @@ SAVEHIST=1000
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt EXTENDED_HISTORY
 setopt SHARE_HISTORY
+
+# do not store any command containing password in history
+function zshaddhistory() {
+  emulate -L zsh
+  if [[ $1 = *"password"* ]]; then
+    return 1
+  fi
+}
