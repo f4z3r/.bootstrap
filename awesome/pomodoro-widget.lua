469da0f80bf78988c13608ff422423b3f1b6a049
Jakob Beckmann <jakobbeckmann@pm.me>
2022-06-07 20:01:50 +0200

    feat(awesome): integrate pomodoro better in awesome and provide information on waiting times

--- awesome/pomodoro-widget.lua
+++ awesome/pomodoro-widget.lua
@@ -23,6 +23,7 @@ function Pomodoro:new(args)
   obj.current_duration     = obj.work_duration
 
   obj.last_start = 0
+  obj.finished   = false
 
   obj.log           = '/tmp/pomodoro.log'
   obj.notif_done    = "Completed! Time for a break..."
@@ -38,7 +39,7 @@ function Pomodoro:new(args)
   obj.color         = "#ACA1A1"
   obj.color_working = "#DF0101"
   obj.color_pausing = "#00B700"
-  obj.current_color = obj.color_working
+  obj.current_color = obj.color_pausing
 
   obj.symbol_running    = "\u{e003}"
   obj.symbol_pause      = "\u{e005}"
@@ -57,7 +58,8 @@ function Pomodoro:new(args)
   obj.widget:set_font(args.font)
   obj.widget:set_markup(obj.format("25:00", obj.current_color, obj.current_symbol, obj.completed))
 
-  obj.timer = timer { timeout = 5 }
+  obj.timeout = 5
+  obj.timer = timer { timeout = obj.timeout }
 
   obj.timer:connect_signal("timeout", function () obj:update() end)
 
@@ -74,48 +76,52 @@ function Pomodoro:update()
   local time_running = current_time - self.last_start
   local time_left = self.current_duration - time_running
 
-  if time_left < 0 then
-    self.timer:stop()
-    self.last_start = 0
-    time_left = 0
+  local prefix = ""
+  if time_left <= 0 and time_left > -self.timeout then
     local notif = self.notif_go_back
+    local notif_color = self.color_working
+    self.finished = true
     if self.working then
       self:add()
       notif = self.notif_done
-      self.current_color = self.color_pausing
-    else
-      self.current_color = self.color_working
+      notif_color = self.color_pausing
     end
-    self:notify(notif)
+    self:notify(notif, notif_color)
     self.current_symbol = self.symbol_stopped
+    self.current_color = self.color_pausing
     self.working = not self.working
     self.running = false
   end
+  if time_left < 0 then
+    prefix = "-"
+    time_left = -time_left
+  end
 
-  formated_time = os.date("%M:%S", time_left)
-  self.widget:set_markup(self.format(formated_time, self.current_color, self.current_symbol, self.completed))
+  local formated_time = os.date("%M:%S", time_left)
+  self.widget:set_markup(self.format(prefix .. formated_time, self.current_color, self.current_symbol, self.completed))
 end
 
 
-function Pomodoro:notify(t)
+function Pomodoro:notify(t, c)
   naughty.notify {
-    preset = self.notification_preset,
-    title  = "Pomodoro",
-    text   = markup.font(self.notification_preset.font,
+    preset   = self.notification_preset,
+    timeout  = 0,
+    bg       = c,
+    position = "top_middle",
+    title    = "Pomodoro",
+    text     = markup.font(self.notification_preset.font,
       awful.util.escape(t:gsub("\n*$", "")))
   }
 end
 
 function Pomodoro:skip_break()
-  if self.running then
-    self.timer:stop()
-  end
   self.current_duration = self.work_duration
   self.current_color    = self.color_working
   self.current_symbol   = self.symbol_running
-  self.running          = false
+  self.running          = true
   self.working          = true
-  self.last_start       = 0
+  self.last_start       = os.time()
+  self.finished         = false
   self:update()
 end
 
@@ -123,19 +129,23 @@ function Pomodoro:toggle()
   if self.running then
     self.timer:stop()
     self.current_symbol = self.symbol_stopped
+    self.current_color = self.color_pausing
     self:update()
     local current_time = os.time()
     self.ran = current_time - self.last_start
     self.running = false
-  elseif self.last_start == 0 then
+  elseif self.finished then
+    self.current_color = self.color_working
     self:start()
   else
     self.last_start = os.time() - self.ran
     self.ran = 1
     self.timer:start()
     if self.working then
+      self.current_color = self.color_working
       self.current_symbol = self.symbol_running
     else
+      self.current_color = self.color_pausing
       if (self.completed % 4) == 0 then
         self.current_symbol = self.symbol_long_pause
       else
@@ -149,9 +159,10 @@ end
 
 function Pomodoro:start()
   self.current_duration = self.work_duration
-  self.current_color = self.color_working
-  self.current_symbol = self.symbol_running
-  self.running = true
+  self.current_color    = self.color_working
+  self.current_symbol   = self.symbol_running
+  self.running          = true
+  self.finished         = false
 
   if not self.working then
     self.current_color = self.color_pausing
@@ -180,7 +191,7 @@ function Pomodoro:completed()
 end
 
 function Pomodoro:add()
-  out = io.open(self.log, 'a')
+  local out = io.open(self.log, 'a')
   out:write("\nPomodoro auto ended at " .. os.date("%a %d %b %Y %T") .. ', duration: 25 minutes')
   io.close(out)
   self.completed = self.completed + 1
