c564cecfb739e833900b0ad8a056cc4535b09a37
Jakob Beckmann <jakobbeckmann@pm.me>
2023-03-11 15:28:32 +0100

    feat: install rtx directly via AUR instead of manually

--- zsh/zsh/rtx.zsh
+++ zsh/zsh/rtx.zsh
@@ -4,24 +4,24 @@ rtx() {
   local command
   command="${1:-}"
   if [ "$#" = 0 ]; then
-    command /home/jakob/.local/bin/rtx
+    command /usr/bin/rtx
     return
   fi
   shift
 
   case "$command" in
   deactivate|shell)
-    eval "$(/home/jakob/.local/bin/rtx "$command" "$@")"
+    eval "$(/usr/bin/rtx "$command" "$@")"
     ;;
   *)
-    command /home/jakob/.local/bin/rtx "$command" "$@"
+    command /usr/bin/rtx "$command" "$@"
     ;;
   esac
 }
 
 _rtx_hook() {
   trap -- '' SIGINT;
-  eval "$("/home/jakob/.local/bin/rtx" hook-env -s zsh)";
+  eval "$("/usr/bin/rtx" hook-env -s zsh)";
   trap - SIGINT;
 }
 typeset -ag precmd_functions;
@@ -32,3 +32,4 @@ typeset -ag chpwd_functions;
 if [[ -z "${chpwd_functions[(r)_rtx_hook]+1}" ]]; then
   chpwd_functions=( _rtx_hook ${chpwd_functions[@]} )
 fi
+
